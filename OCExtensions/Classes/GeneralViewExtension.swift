1f1057935c75096a1981d5420b84daf3e07899c7
Henrique Morbin <morbin_@hotmail.com>
2016-08-07 14:29:52 -0300

    Adding centerSuperview to centralize the view in superview

--- OCExtensions/Classes/GeneralViewExtension.swift
+++ OCExtensions/Classes/GeneralViewExtension.swift
@@ -30,12 +30,35 @@ public extension UIView {
         }
     }
     
-    public func fill(inSuperview superview: UIView) {
+    public func fillInSuperview(superview: UIView) {
         if superview.subviews.indexOf(self) == nil {
             superview.addSubview(self)
         }
         
         self.fillSuperview()
+    }
+    
+    public func centerSuperview(withSize size: CGSize?) {
+        if let superview = self.superview {
+            let subview = self
+            subview.translatesAutoresizingMaskIntoConstraints = false
+            
+            superview.addConstraint(NSLayoutConstraint(item: subview, attribute: .CenterX, relatedBy: .Equal, toItem: superview, attribute: .CenterX, multiplier: 1, constant: 0))
+            superview.addConstraint(NSLayoutConstraint(item: subview, attribute: .CenterY, relatedBy: .Equal, toItem: superview, attribute: .CenterY, multiplier: 1, constant: 0))
+            
+            if let size = size {
+                subview.addConstraint(NSLayoutConstraint(item: subview, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: size.width))
+                subview.addConstraint(NSLayoutConstraint(item: subview, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1, constant: size.height))
+            }
+        }
+    }
+    
+    public func centerInSuperview(superview: UIView, withSize size: CGSize?) {
+        if superview.subviews.indexOf(self) == nil {
+            superview.addSubview(self)
+        }
+        
+        self.centerSuperview(withSize: size)
     }
     
 }
