25ca4432cea4b036ef00a54df1ce212a6aa56933
Henrique Morbin <morbin_@hotmail.com>
2016-08-14 18:51:29 -0300

    Adding Non Optional text property

---
+++ OCExtensions/Classes/TextField/GeneralTextFieldExtensions.swift
@@ -0,0 +1,17 @@
+//
+//  GeneralTextFieldExtensions.swift
+//  Pods
+//
+//  Created by Henrique Morbin on 14/08/16.
+//
+//
+
+import UIKit
+
+extension UITextField {
+    
+    public var textValue: String {
+        return text ?? ""
+    }
+    
+}
