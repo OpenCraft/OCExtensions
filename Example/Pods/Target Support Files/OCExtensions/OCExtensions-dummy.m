875b01e4f486e9925476558dded86927eaa08a01
Henrique Morbin <morbin_@hotmail.com>
2016-12-18 20:16:59 -0200

    Removing pod folder

--- Example/Pods/Target Support Files/OCExtensions/OCExtensions-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_OCExtensions : NSObject
-@end
-@implementation PodsDummy_OCExtensions
-@end
--- Example/Pods/Target Support Files/Pods-OCExtensions_Example/Pods-OCExtensions_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OCExtensions_Example : NSObject
-@end
-@implementation PodsDummy_Pods_OCExtensions_Example
-@end
