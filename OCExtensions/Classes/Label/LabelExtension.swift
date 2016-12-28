bb867ed5aa353727e1acebdb39e5b9e1ba31b946
cristianDRM <cristianmadridd@gmail.com>
2016-12-28 10:57:40 -0200

    add extension to copy attributes and set text

---
+++ OCExtensions/Classes/Label/LabelExtension.swift
@@ -0,0 +1,18 @@
+//
+//  LabelExtension.swift
+//  Pods
+//
+//  Created by Cristian Madrid on 12/27/16.
+//
+//
+
+import UIKit
+
+public extension UILabel {
+    
+    public func setAttributedText(title: String) {
+        
+        var range = NSRange(location: 1, length: 1)
+        attributedText = NSMutableAttributedString(string: title, attributes: attributedText?.attributes(at: 0, effectiveRange: &range))
+    }
+}
