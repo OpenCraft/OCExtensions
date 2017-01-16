009a4d26cd7e3fbefb0f78b0aa3c48b77f4a6942
Uriel Battanoli <urielbattanoli@hotmail.com>
2017-01-16 20:33:18 -0200

    Adding NSString Extension Adding String Extension

---
+++ OCExtensions/Classes/String/StringExtension.swift
@@ -0,0 +1,27 @@
+//
+//  StringExtension.swift
+//  Pods
+//
+//  Created by Uriel Battanoli on 1/16/17.
+//
+//
+
+import Foundation
+
+extension String {
+    func sizeOfText(for font: UIFont, width: Int) -> CGSize{
+        return NSString(string: self).sizeOfText(for: font, width: width)
+    }
+    
+    func sizeOfText(for font: UIFont, height: Int) -> CGSize{
+        return NSString(string: self).sizeOfText(for: font, height: height)
+    }
+    
+    func heightOfText(for font: UIFont, width: Int, minHeight: Int = 0, margin: Int = 4) -> Int{
+        return NSString(string: self).heightOfText(for: font, width: width, minHeight: minHeight, margin: margin)
+    }
+    
+    func widthOfText(for font: UIFont, height: Int, minWidth: Int = 0, margin: Int = 4) -> Int{
+        return NSString(string: self).widthOfText(for: font, height: height, minWidth: minWidth, margin: margin)
+    }
+}
