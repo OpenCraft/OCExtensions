e097df7f9543f2a2c8a9d696d7859042d3f45c5a
Uriel Battanoli <urielbattanoli@hotmail.com>
2017-01-17 10:38:28 -0200

    Adding label textValue

--- OCExtensions/Classes/NSString/NSStringExtension.swift
+++ OCExtensions/Classes/NSString/NSStringExtension.swift
@@ -10,23 +10,23 @@ import Foundation
 
 extension NSString {
     
-    func sizeOfText(for font: UIFont, width: Int) -> CGSize{
+    func sizeOfText(for font: UIFont, width: Int) -> CGSize {
         let size = CGSize(width: width, height: Int.max)
         let att = [NSFontAttributeName : font]
         return self.boundingRect(with: size, options: .usesLineFragmentOrigin, attributes: att, context: nil).size
     }
     
-    func sizeOfText(for font: UIFont, height: Int) -> CGSize{
+    func sizeOfText(for font: UIFont, height: Int) -> CGSize {
         let size = CGSize(width: Int.max, height: height)
         let att = [NSFontAttributeName : font]
         return self.boundingRect(with: size, options: .usesLineFragmentOrigin, attributes: att, context: nil).size
     }
     
-    func heightOfText(for font: UIFont, width: Int, minHeight: Int = 0, margin: Int = 4) -> Int{
+    func heightOfText(for font: UIFont, width: Int, minHeight: Int = 0, margin: Int = 4) -> Int {
         return max(minHeight, Int(sizeOfText(for: font, width: width).height) + margin)
     }
     
-    func widthOfText(for font: UIFont, height: Int, minWidth: Int = 0, margin: Int = 4) -> Int{
+    func widthOfText(for font: UIFont, height: Int, minWidth: Int = 0, margin: Int = 4) -> Int {
         return max(minWidth, Int(sizeOfText(for: font, height: height).width) + margin)
     }
 }
--- OCExtensions/Classes/String/StringExtension.swift
+++ OCExtensions/Classes/String/StringExtension.swift
@@ -10,19 +10,19 @@ import Foundation
 
 extension String {
     
-    func sizeOfText(for font: UIFont, width: Int) -> CGSize{
+    func sizeOfText(for font: UIFont, width: Int) -> CGSize {
         return NSString(string: self).sizeOfText(for: font, width: width)
     }
     
-    func sizeOfText(for font: UIFont, height: Int) -> CGSize{
+    func sizeOfText(for font: UIFont, height: Int) -> CGSize {
         return NSString(string: self).sizeOfText(for: font, height: height)
     }
     
-    func heightOfText(for font: UIFont, width: Int, minHeight: Int = 0, margin: Int = 4) -> Int{
+    func heightOfText(for font: UIFont, width: Int, minHeight: Int = 0, margin: Int = 4) -> Int {
         return NSString(string: self).heightOfText(for: font, width: width, minHeight: minHeight, margin: margin)
     }
     
-    func widthOfText(for font: UIFont, height: Int, minWidth: Int = 0, margin: Int = 4) -> Int{
+    func widthOfText(for font: UIFont, height: Int, minWidth: Int = 0, margin: Int = 4) -> Int {
         return NSString(string: self).widthOfText(for: font, height: height, minWidth: minWidth, margin: margin)
     }
 }
